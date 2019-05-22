e3d60d79ea0c1d7f3abd58b59f5002d498317e66
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-22 16:36:24 +0530

    update tabs state using effects

--- src/tab-explorer.jsx
+++ src/tab-explorer.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { render } from 'react-dom'
 import 'tachyons'
 import './main.css'
+import mergeDeepLeft from 'ramda/es/mergeDeepLeft'
 
 console.log('tab-explorer.js loaded')
 
@@ -15,19 +16,54 @@ console.log('tab-explorer.js loaded')
 //   })
 // })
 
-function logCurrent() {
-  chrome.windows.getCurrent({ populate: true }, w =>
-    console.log('current window', w.id, w),
+// function logCurrent() {
+//   chrome.windows.getCurrent({ populate: true }, w =>
+//     console.log('current window', w.id, w),
+//   )
+//   chrome.tabs.getCurrent(t => console.log('current tab', t.id, t))
+// }
+
+// logCurrent()
+
+const App = () => {
+  const [state, setState] = useState(() => ({ tabId: -1, tabs: [] }))
+
+  useEffect(() => {
+    chrome.windows.getCurrent({ populate: true }, w => {
+      console.log('current window', w.id, w)
+      chrome.tabs.getCurrent(t => {
+        setState(mergeDeepLeft({ tabId: t.id, tabs: w.tabs }))
+        console.log('current tab', t.id, t)
+      })
+    })
+  }, [setState])
+
+  useEffect(() => {
+    const currTabId = state.tabId
+    console.log('eff called')
+
+    if (currTabId === -1) return
+
+    console.log('eff registering listener')
+
+    const activatedListener = ({ tabId }) => {
+      // if (currTabId !== tabId) return
+      chrome.windows.getCurrent({ populate: true }, w => {
+        setState(mergeDeepLeft({ tabId: currTabId, tabs: w.tabs }))
+      })
+    }
+    chrome.tabs.onActivated.addListener(activatedListener)
+
+    return () => {
+      chrome.tabs.onActivated.removeListener(activatedListener)
+    }
+  }, [state.tabId, setState])
+
+  return (
+    <div>
+      <div>HW</div>
+    </div>
   )
-  chrome.tabs.getCurrent(t => console.log('current tab', t.id, t))
 }
-
-logCurrent()
-
-const App = () => (
-  <div>
-    <div>HW</div>
-  </div>
-)
 
 render(<App />, document.getElementById('root'))
