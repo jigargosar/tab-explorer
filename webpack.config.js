62febebd0fb343ab98e0f526de6c166c83584072
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-22 12:41:42 +0530

    clean manifest json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const pth = path.resolve
+const R = require('ramda')
+
+const cleanManifest = R.pipe(
+  R.curryN(1, JSON.parse),
+  R.omit(['$schema']),
+  R.partialRight(JSON.stringify, [null, 2]),
+)
 
 module.exports = {
   context: pth(__dirname),
@@ -9,7 +16,10 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin([
-      { from: pth('src/manifest.json') },
+      {
+        from: pth('src/manifest.json'),
+        transform: cleanManifest,
+      },
       { from: pth('src/tab-explorer.html') },
     ]),
   ],
