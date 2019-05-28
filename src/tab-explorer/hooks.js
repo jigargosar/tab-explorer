bb9163564770f897e63dc4daf8bdb8547ae5a083
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-28 16:51:58 +0530

    use modifiedAt to sync sessions with firebase

--- src/tab-explorer/hooks.js
+++ src/tab-explorer/hooks.js
@@ -6,7 +6,6 @@ import {
   useCallback,
   useMemo,
   createContext,
-  // useRef,
 } from 'react'
 import over from 'ramda/es/over'
 import nanoid from 'nanoid'
@@ -16,14 +15,12 @@ import startsWith from 'ramda/es/startsWith'
 import propSatisfies from 'ramda/es/propSatisfies'
 import mergeDeepRight from 'ramda/es/mergeDeepRight'
 import defaultTo from 'ramda/es/defaultTo'
-import omit from 'ramda/es/omit'
 import lensPath from 'ramda/es/lensPath'
 import equals from 'ramda/es/equals'
 import { getCache, setCache, overProp, mergeModel } from './basics'
 import not from 'ramda/es/not'
 import map from 'ramda/es/map'
 import assoc from 'ramda/es/assoc'
-// import PouchDB from 'pouchdb-browser'
 import { pipe } from './safe-basics'
 import pick from 'ramda/es/pick'
 import firebase from 'firebase/app'
@@ -94,57 +91,6 @@ const useCurrentWindowTabs = () => {
   return tabs
 }
 
-// class Session {
-//   constructor(id, createdAt, tabs) {
-//     this.id = id
-//     this.createdAt = createdAt
-//     this.tabs = tabs
-//   }
-//   encode() {
-//     const { id, createdAt, tabs } = this
-//     return { id, createdAt, tabs }
-//   }
-//   static encode(session) {
-//     return session.encode()
-//   }
-//   static decode({ id, createdAt, tabs }) {
-//     return new Session(id, createdAt, tabs)
-//   }
-//   static newFromTabs(tabs) {
-//     return new Session('S_' + nanoid(), Date.now(), tabs)
-//   }
-// }
-//
-// class SessionStore {
-//   constructor(byId) {
-//     this.byId = byId
-//   }
-//   static empty() {
-//     return new SessionStore({})
-//   }
-//   static encode(store) {
-//     return Object.values(store.byId).map(Session.encode)
-//   }
-//   static decode(sessionAttrList) {
-//     const byId = sessionAttrList.reduce((byId, attrs) => {
-//       byId[attrs.id] = Session.decode(attrs)
-//       return byId
-//     }, {})
-//     return new SessionStore(byId)
-//   }
-
-//   mapById(fn) {
-//     return new SessionStore(fn(this.byId))
-//   }
-
-//   static createAndAddNewSessionFromTabs(tabs, ss) {
-//     const session = Session.newFromTabs(tabs)
-//     return ss.mapById(mergeModel(session))
-//   }
-// }
-
-// const overSessionStore = overProp('sessionStore')
-
 const loadCachedState = () => {
   const defaultState = { sessions: {} }
   const stateProps = Object.keys(defaultState)
@@ -167,10 +113,7 @@ const loadCachedState = () => {
 }
 
 function encodeState(state) {
-  const fn = pipe(
-    // overSessionStore(SessionStore.encode),
-    state => JSON.stringify(state, null, 2),
-  )
+  const fn = pipe(state => JSON.stringify(state, null, 2))
   return fn(state)
 }
 
@@ -201,14 +144,10 @@ function useActions(setState) {
   return useMemo(() => {
     const setStateProp = prop => fn => setState(overProp(prop)(fn))
     const setSessions = setStateProp('sessions')
-    // const setSessionStore = setStateProp('sessionStore')
 
     function createAndAddSessionFromTabs(tabs) {
       const session = sessionFromTabs(tabs)
       setSessions(mergeModel(session))
-      // setSessionStore(ss =>
-      //   SessionStore.createAndAddNewSessionFromTabs(tabs, ss),
-      // )
     }
 
     return {
@@ -242,10 +181,6 @@ function useActions(setState) {
       deleteSessionTab: (sessionId, tab) => {
         setSessions(overPath([sessionId, 'tabs'])(reject(equals(tab))))
       },
-      // addNewSessionFromTabs: tabs => {
-      //   const session = sessionFromTabs(tabs)
-      //   setSessions(mergeModel(session))
-      // },
       onOpenTabsClicked: tabs => {
         tabs.forEach(createTab)
       },
@@ -264,35 +199,6 @@ function useActions(setState) {
     }
   }, [setState])
 }
-
-// function pouchDbPersistAppState(state, db) {
-//   return db
-//     .get('root')
-//     .then(rootDoc => {
-//       console.log('rootDoc :', rootDoc)
-//     })
-//     .catch(() => {
-//       return db.put({ _id: 'root', state, createdAt: Date.now() })
-//     })
-// }
-
-// function useCachePouchDBEffect(state) {
-//   const pdbRef = useRef()
-//   useEffect(() => {
-//     const db = new PouchDB('tab-explorer-app-state')
-//     pdbRef.current = db
-//     return () => {
-//       db.close()
-//       pdbRef.current = null
-//     }
-//   }, [])
-
-//   useEffect(() => {
-//     const db = pdbRef.current
-//     if (!db) return
-//     pouchDbPersistAppState(state, db)
-//   }, [state, pdbRef.current])
-// }
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBVS1Tx23pScQz9w4ZDTGh307mqkCRy2Bw',
