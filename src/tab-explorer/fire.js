94cee9f79cf3334291c779615c31cc0234b5d048
Jigar Gosar <jigar.gosar@gmail.com>
2019-05-30 17:23:50 +0530

    fire sync

--- src/tab-explorer/fire.js
+++ src/tab-explorer/fire.js
@@ -3,10 +3,18 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { SessionStore } from './session-store'
 import { pipe } from './safe-basics'
 import map from 'ramda/es/map'
+import equals from 'ramda/es/equals'
+import keys from 'ramda/es/keys'
+import isEmpty from 'ramda/es/isEmpty'
+import differenceWith from 'ramda/es/differenceWith'
+import values from 'ramda/es/values'
+import difference from 'ramda/es/difference'
+import pluck from 'ramda/es/pluck'
+import tap from 'ramda/es/tap'
 
 export const signIn = () => {
   const auth = firebase.auth()
@@ -35,6 +43,19 @@ export function useAuth() {
   return useAuthState(firebase.auth())
 }
 
+function usePrevious(value) {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref = useRef()
+
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value
+  }, [value]) // Only re-run if value changes
+
+  // Return previous value (happens before update in useEffect above)
+  return ref.current
+}
 export function syncSessions(actions, sessionStore) {
   const [user] = useAuth()
 
@@ -42,32 +63,45 @@ export function syncSessions(actions, sessionStore) {
     if (!user) return
     const sessionsCRef = getSessionsCRef(user)
     const disposer = sessionsCRef.onSnapshot(qs => {
-      console.log('fire: Sessions Changed', qs.docs.length, qs)
-      actions.updateSessionsIfNewer(qs.docs.map(ds => ds.data()))
+      console.log(
+        'fire: Sessions Changed',
+        qs.docChanges({}),
+        qs.docs.length,
+        qs,
+      )
+      const fireSessions = qs.docs.map(ds => ds.data())
+      actions.updateSessionsIfNewer(fireSessions)
     }, console.error)
     return disposer
   }, [user])
 
-  const sessionLookup = SessionStore.toIdLookup(sessionStore)
-
+  const sLookup = SessionStore.toIdLookup(sessionStore)
+  const prevSLookup = usePrevious(sLookup)
   useEffect(() => {
-    if (!user || !sessionLookup) return
+    if (!user || !sLookup) return
+    const changedSessions = difference(values(sLookup))(
+      values(prevSLookup),
+    )
+    const sessionIds = pluck('id')(changedSessions)
+
+    if (isEmpty(sessionIds)) return
+
     const sessionsCRef = getSessionsCRef(user)
     firebase
       .firestore()
       .runTransaction(async t => {
         const fetchDocWithSessionId = sid => t.get(sessionsCRef.doc(sid))
 
-        const fetchDocsFromSessionLookup = pipe(
-          Object.keys,
+        const fetchSessionsFromIds = pipe(
           map(fetchDocWithSessionId),
-          Promise.all,
+          tap(console.log),
+          Promise.all.bind(Promise),
         )
 
-        const docSnaps = await fetchDocsFromSessionLookup(sessionLookup)
+        const docSnaps = await fetchSessionsFromIds(sessionIds)
 
         docSnaps.forEach(snap => {
-          const session = sessionLookup[snap.id]
+          const session = sLookup[snap.id]
           snap.exists
             ? t.update(snap.ref, session)
             : t.set(snap.ref, session)
@@ -77,11 +111,10 @@ export function syncSessions(actions, sessionStore) {
         console.log('fire: write all docs transaction success. '),
       )
       .catch(console.error)
-  }, [user, sessionLookup])
+  }, [user, sLookup])
 }
 
 function getSessionsCRef(user) {
   const db = firebase.firestore()
-  const sref = db.collection(`users/${user.uid}/tab-ex-sessions`)
-  return sref
+  return db.collection(`users/${user.uid}/tab-ex-sessions`)
 }
