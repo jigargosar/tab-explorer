849d29f67ff73a010ec8858ea238c54a7a7629de
Jigar Gosar <jigar.gosar@gmail.com>
2019-06-07 17:22:40 +0530

    using elm for better state management

--- src/TabExplorer.elm
+++ src/TabExplorer.elm
@@ -9,55 +9,6 @@ import Json.Encode as JE exposing (Value)
 port onCurrentWindowTabsChanged : (JE.Value -> msg) -> Sub msg
 
 
-main : Program Flags Model Msg
-main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.batch [ onCurrentWindowTabsChanged OnCurrentWindowTabs ]
-
-
-type alias Flags =
-    {}
-
-
-type alias Model =
-    {}
-
-
-init : Flags -> ( Model, Cmd Msg )
-init flags =
-    ( {}, Cmd.none )
-
-
-type Msg
-    = NoOp
-    | OnCurrentWindowTabs JE.Value
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-        OnCurrentWindowTabs encodedTabs ->
-            let
-                _ =
-                    encodedTabs
-                        |> JD.decodeValue (JD.list tabDecoder)
-                        |> Debug.log "encodedTabs"
-            in
-            ( model, Cmd.none )
-
-
 type alias Tab =
     { id : Int
     , title : String
@@ -75,6 +26,58 @@ tabDecoder =
         (JD.maybe <| JD.field "favIconUrl" JD.string)
 
 
+type alias Flags =
+    {}
+
+
+type alias Model =
+    { openTabs : List Tab }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { openTabs = [] }, Cmd.none )
+
+
+type Msg
+    = NoOp
+    | OnCurrentWindowTabs JE.Value
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch [ onCurrentWindowTabsChanged OnCurrentWindowTabs ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        OnCurrentWindowTabs encodedTabs ->
+            let
+                newModel =
+                    encodedTabs
+                        |> JD.decodeValue (JD.list tabDecoder)
+                        |> Debug.log "encodedTabs"
+                        |> Result.map
+                            (\tabs -> { model | openTabs = tabs })
+                        |> Result.withDefault model
+            in
+            ( newModel, Cmd.none )
+
+
 view : Model -> Html Msg
 view model =
     div [] [ text "foo" ]
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
